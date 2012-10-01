ed882224d2ac9d30ee460b1d0f0877e30e097c3e
Brett O'Donnell <cornernote@gmail.com>
2012-10-01 14:20:13 +0930

    added deploy arrows

--- deploy_spiral/init.lua
+++ deploy_spiral/init.lua
@@ -161,6 +161,84 @@ deploy_spiral.register = function(label,name,material,texture)
 		},
 	})
 
+	-- arrow
+	table.insert(arrows,{"deploy_spiral:"..name.."_arrow", "deploy_spiral:"..name.."_arrow_entity"})
+	minetest.register_craftitem("deploy_spiral:"..name.."_arrow", {
+		description = label.." Spiral Arrow",
+		inventory_image = "deploy_spiral_arrow.png",
+	})
+	minetest.register_node("deploy_spiral:"..name.."_arrow_box", {
+		drawtype = "nodebox",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				-- Shaft
+				{-6.5/17, -1.5/17, -1.5/17, 6.5/17, 1.5/17, 1.5/17},
+				--Spitze
+				{-4.5/17, 2.5/17, 2.5/17, -3.5/17, -2.5/17, -2.5/17},
+				{-8.5/17, 0.5/17, 0.5/17, -6.5/17, -0.5/17, -0.5/17},
+				--Federn
+				{6.5/17, 1.5/17, 1.5/17, 7.5/17, 2.5/17, 2.5/17},
+				{7.5/17, -2.5/17, 2.5/17, 6.5/17, -1.5/17, 1.5/17},
+				{7.5/17, 2.5/17, -2.5/17, 6.5/17, 1.5/17, -1.5/17},
+				{6.5/17, -1.5/17, -1.5/17, 7.5/17, -2.5/17, -2.5/17},
+				{7.5/17, 2.5/17, 2.5/17, 8.5/17, 3.5/17, 3.5/17},
+				{8.5/17, -3.5/17, 3.5/17, 7.5/17, -2.5/17, 2.5/17},
+				{8.5/17, 3.5/17, -3.5/17, 7.5/17, 2.5/17, -2.5/17},
+				{7.5/17, -2.5/17, -2.5/17, 8.5/17, -3.5/17, -3.5/17},
+			}
+		},
+		tiles = {"deploy_spiral_arrow.png", "deploy_spiral_arrow.png", "deploy_spiral_arrow_back.png", "deploy_spiral_arrow_front.png", "deploy_spiral_arrow_2.png", "deploy_spiral_arrow.png"},
+	})
+	minetest.register_entity("deploy_spiral:"..name.."_arrow_entity", {
+		physical = false,
+		timer=0,
+		visual = "wielditem",
+		visual_size = {x=0.1, y=0.1},
+		textures = {"deploy_spiral:"..name.."_arrow_box"},
+		lastpos={},
+		collisionbox = {0,0,0,0,0,0},
+		player = "",
+		on_step = function(self, dtime)
+			self.timer=self.timer+dtime
+			local pos = self.object:getpos()
+			local node = minetest.env:get_node(pos)
+			if self.timer>0.2 then
+				local objs = minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 2)
+				for k, obj in pairs(objs) do
+					if obj:get_luaentity() ~= nil then
+						if obj:get_luaentity().name ~= "deploy_spiral:"..name.."_arrow_entity" and obj:get_luaentity().name ~= "__builtin:item" then
+							if self.player ~= "" then
+								self.player:setpos(pos)
+								self.player:get_inventory():add_item("main", ItemStack("deploy_spiral:"..name.."_arrow"))
+							end
+							self.object:remove()
+						end
+					else
+						if self.player ~= "" then
+							self.player:setpos(pos)
+							self.player:get_inventory():add_item("main", ItemStack("deploy_spiral:"..name.."_arrow"))
+						end
+						self.object:remove()
+					end
+				end
+			end
+			if self.lastpos.x~=nil then
+				if node.name ~= "air" then
+					deploy_spiral.deploy(self.lastpos,self.player,material,3,1,1)
+					self.object:remove()
+				end
+			end
+			self.lastpos={x=pos.x, y=pos.y, z=pos.z}
+		end,
+	})
+	minetest.register_craft({
+		output = "deploy_spiral:"..name.."_arrow",
+		recipe = {
+			{"default:stick", "default:stick", "deploy_spiral:"..name.."_small"},
+		}
+	})
+	
 end
 
 
